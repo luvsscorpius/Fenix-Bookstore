9c53f568bb2a42d10709a3f8aad98ccac45e1223
and <andersonvittor1801@gmail.com>
2023-05-01 22:08:57 -0300

    Quarto commit

--- js/script.js
+++ js/script.js
@@ -27,6 +27,11 @@ const preencherDadosDoItem = (itemElement, item, index) => {
 // Precisamos mapear os livros
 produtosJson.map((item, index) => {
     let romanceItem = document.querySelector('.models .book-item').cloneNode(true) // cloneNode faz uma copia dos elementos
+    let fantasiaItem = document.querySelector('.models .book-item').cloneNode(true)
+    let infatisItem = document.querySelector('.models .book-item').cloneNode(true)
+    let idiomasItem = document.querySelector('.models .book-item').cloneNode(true)
+    let hqsmangasItem = document.querySelector('.models .book-item').cloneNode(true)
+    let espiritualidadeItem = document.querySelector('.models .book-item').cloneNode(true)
 
     if (item.type == "Romance") {
         //agora que ja foi clonado precisamos colocar na main e fazer aparecer na tela
@@ -34,5 +39,35 @@ produtosJson.map((item, index) => {
 
         // agora que ja colocamos na main precisamos preencher os dados
         preencherDadosDoItem(romanceItem, item, index)
+    }
+
+    if (item.type == "Fantasia") {
+        seleciona('.fantasia-area').append(fantasiaItem)
+
+        preencherDadosDoItem(fantasiaItem, item, index)
+    }
+
+    if (item.type == 'infantis') {
+        seleciona('.infatis-area').append(infatisItem)
+
+        preencherDadosDoItem(infatisItem, item, index)
+    }
+
+    if (item.type == "idiomas") {
+        seleciona('.idiomas-area').append(idiomasItem)
+
+        preencherDadosDoItem(idiomasItem, item, index)
+    }
+
+    if (item.type == "hqs-mangas") {
+        seleciona('.hqs-area').append(hqsmangasItem)
+
+        preencherDadosDoItem(hqsmangasItem, item, index)
+    }
+
+    if (item.type == "espiritualidade") {
+        seleciona('.espiritualidade-area').append(espiritualidadeItem)
+
+        preencherDadosDoItem(espiritualidadeItem, item, index)
     }
 })
