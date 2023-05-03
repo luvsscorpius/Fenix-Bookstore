bf6d9c0cdfe0ea71364df7e7dfc49189735d0b64
Anderson Vitor Feliciano Da Silva <886937@senacminas.edu.br>
2023-05-03 09:21:58 -0300

    Sexto commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 //funcoes auxiliares e uteis
 const seleciona = (elemento) => document.querySelector(elemento)
-const selecionaTodos = (elemento) => document.querySelectorall(elemento)
+const selecionaTodos = (elemento) => document.querySelectorAll(elemento)
 
 // converter para real
 const formatoReal = (valor) => {
@@ -14,6 +14,28 @@ const formatoMonetario = (valor) => {
     }
 }
 
+// Para abrir o modal
+const abrirModal = () => {
+    seleciona('.booksWindowArea').style.opacity = 0
+    seleciona('.booksWindowArea').style.display = 'flex'
+    setTimeout(() => { seleciona('.booksWindowArea').style.opacity = 1}, 150)
+    // set timeout foi usado para dar animaç~çao na janela modal
+}
+
+// Função para fechar a janela modal
+const fecharModal = () => {
+    seleciona('.booksWindowArea').style.opacity = 0
+    setTimeout(() => {
+        seleciona('.booksWindowArea').style.display = 'none'
+    }, 500)
+}
+
+const botoesFechar = () => {
+    selecionaTodos('.booksInfo--cancelMobileButton, .booksInfo--cancelButton').forEach((item) => {
+        item.addEventListener('click', fecharModal)
+    })
+}
+
 // funcao para preencher os dados dos livros na main
 const preencherDadosDoItem = (itemElement, item, index) => {
     itemElement.setAttribute('data-key', index)
@@ -24,6 +46,16 @@ const preencherDadosDoItem = (itemElement, item, index) => {
     itemElement.querySelector('.book-item--price').innerHTML = formatoReal(item.price[1])
 }
 
+// funcao para preencher os dados no modal
+const preencherDadosModal = (item) => {
+    let id = seleciona('.book-item--id').innerHTML = item.id -1
+    seleciona('.booksBig img').src = item.img
+    seleciona('.booksInfo h1').innerHTML = item.name
+    seleciona('.booksInfo--author').innerHTML = item.author
+    seleciona('.booksInfo--desc').innerHTML = item.description
+    seleciona('.booksInfo--actualPrice').innerHTML = formatoReal(item.price[2])
+}
+
 // Precisamos mapear os livros
 produtosJson.map((item, index) => {
     let romanceItem = document.querySelector('.models .book-item').cloneNode(true) // cloneNode faz uma copia dos elementos
@@ -32,6 +64,7 @@ produtosJson.map((item, index) => {
     let idiomasItem = document.querySelector('.models .book-item').cloneNode(true)
     let hqsmangasItem = document.querySelector('.models .book-item').cloneNode(true)
     let espiritualidadeItem = document.querySelector('.models .book-item').cloneNode(true)
+    let autoAjudaItem = document.querySelector('.models .book-item').cloneNode(true)
 
     if (item.type == "Romance") {
         //agora que ja foi clonado precisamos colocar na main e fazer aparecer na tela
@@ -39,35 +72,109 @@ produtosJson.map((item, index) => {
 
         // agora que ja colocamos na main precisamos preencher os dados
         preencherDadosDoItem(romanceItem, item, index)
+
+        romanceItem.querySelector('.book-item a').addEventListener('click', (e) => {
+            e.preventDefault() // faz o a (link) não fazer o padrao dele que seria recarregar a página
+            console.log('Clicou em um livro da área romance')
+
+            // funcao abrir modal quando clicar em algum livro
+            abrirModal()
+
+            // funcao para preencher os dados dos livros na janela modal
+            preencherDadosModal(item)
+
+            // funcao para fechar a janela modal quando clicar em algum dos botoes de fechar
+            botoesFechar()
+        })
     }
 
     if (item.type == "Fantasia") {
         seleciona('.fantasia-area').append(fantasiaItem)
 
         preencherDadosDoItem(fantasiaItem, item, index)
+
+        fantasiaItem.querySelector('.book-item a').addEventListener('click', (e) => {
+            e.preventDefault()
+            console.log('Clicou em um livro da área fantasia')
+
+            abrirModal()
+            preencherDadosModal(item)
+            botoesFechar()
+        })
     }
 
     if (item.type == 'infantis') {
         seleciona('.infatis-area').append(infatisItem)
 
         preencherDadosDoItem(infatisItem, item, index)
+
+        infatisItem.querySelector('.book-item a').addEventListener('click', (e) => {
+            e.preventDefault()
+            console.log('Clicou em um livro da área infantil')
+
+            abrirModal()
+            preencherDadosModal(item)
+            botoesFechar()
+        })
     }
 
     if (item.type == "idiomas") {
         seleciona('.idiomas-area').append(idiomasItem)
 
         preencherDadosDoItem(idiomasItem, item, index)
+        
+        idiomasItem.querySelector('.book-item a').addEventListener('click', (e) => {
+            e.preventDefault()
+            console.log('Clicou em um livro da área de idiomas')
+
+            abrirModal()
+            preencherDadosModal(item)
+            botoesFechar()
+        })
     }
 
     if (item.type == "hqs-mangas") {
         seleciona('.hqs-area').append(hqsmangasItem)
 
         preencherDadosDoItem(hqsmangasItem, item, index)
+
+        hqsmangasItem.querySelector('.book-item a').addEventListener('click', (e) => {
+            e.preventDefault()
+            console.log('Clicou em um livro da área hqs e mangas')
+
+            abrirModal()
+            preencherDadosModal(item)
+            botoesFechar()
+        })
     }
 
     if (item.type == "espiritualidade") {
         seleciona('.espiritualidade-area').append(espiritualidadeItem)
 
         preencherDadosDoItem(espiritualidadeItem, item, index)
+
+        espiritualidadeItem.querySelector('.book-item a').addEventListener('click', (e) => {
+            e.preventDefault()
+            console.log('Clicou em um livro da área de espiritualidade e religião')
+
+            abrirModal()
+            preencherDadosModal(item)
+            botoesFechar()
+        })
+    }
+
+    if (item.type == "autoajuda") {
+        seleciona('.auto-ajuda-area').append(autoAjudaItem)
+
+        preencherDadosDoItem(autoAjudaItem, item, index)
+
+        autoAjudaItem.querySelector('.book-item a').addEventListener('click', (e) => {
+            e.preventDefault()
+            console.log('Clicou em um livro da área de AutoAjuda')
+
+            abrirModal()
+            preencherDadosModal(item)
+            botoesFechar()
+        })
     }
 })
